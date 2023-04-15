9f7ff187b3e1bdaeb0eae96019d0d86501145376
mfahadarif85 <mfahadarif@gmail.com>
2023-04-15 22:38:21 +0500

    routes & middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 import express from "express";
+import router from "./router";
+import morgan from "morgan";
+import cors from "cors";
+
 const app = express();
+
+const customLogger = (message) => (req, res, next) => {
+  console.log(`Hello from ${message}`);
+  next();
+};
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(customLogger("Custom Logger"));
+app.use(cors());
 
 app.get("/", (req, res) => {
   console.log("hello from express js");
   res.status(200);
   res.json("Hello");
 });
+
+app.use("/api", router);
 
 export default app;
